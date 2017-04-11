a44ef77d63d0012af2182eaff2ab7eff9e4a2e1b
Masayuki <hayashi.masayuki@itolab.nitech.ac.jp>
2017-04-11 19:39:50 +0900

    [update] updated hook.php

--- hook/hook.php
+++ hook/hook.php
@@ -13,8 +13,9 @@ $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
 if ( isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac ) {
     $payload = json_decode($HTTP_RAW_POST_DATA, true);  // 受け取ったJSONデータ
     // ここに実行したいコードを書く
-    exec('git pull 2>&1');
+    $output = shell_exec('git pull 2>&1');
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['after']." ".$payload['commits'][0]['message']."\n", FILE_APPEND|LOCK_EX);
+    file_put_contents($LOG_FILE, $output, FILE_APPEND|LOCK_EX);
 } else {
     // 認証失敗
     file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
