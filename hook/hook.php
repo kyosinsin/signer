cfb5feccd5ab73222dc7acb96b69e5edbab24758
Qiao Sensen <qiaosensen1992@gmail.com>
2017-04-11 10:02:49 +0000

    [update] updated hook.php

--- hook/hook.php
+++ hook/hook.php
@@ -6,7 +6,7 @@
 
 // 設定
 $LOG_FILE = dirname(__FILE__).'/hook.log';
-$SECRET_KEY = file_get_contents(dirname(__FILE__).'/.secret');
+$SECRET_KEY = trim(file_get_contents(dirname(__FILE__).'/.secret'));
 
 $header = getallheaders();
 $hmac = hash_hmac('sha1', $HTTP_RAW_POST_DATA, $SECRET_KEY);
